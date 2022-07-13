764519a91113b9dfd274722596861167e5cc4cda
islom9797 <islom9797@4-lab.com>
2022-07-13 15:27:45 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/islom/portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.islom.portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main/components/side_menu.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/constants.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'area_info_text.dart';
+import 'coding.dart';
+import 'knowledges.dart';
+import 'my_info.dart';
+import 'skills.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SideMenu extends StatelessWidget {
+  void _launchURL() async {
+    if (!await launch(_url)) throw 'Could not launch $_url';
+  }
+
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+  static const String _url = 'https://flutter.dev';
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            MyInfo(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Column(
+                  children: [
+                    AreaInfoText(
+                      title: "Residence",
+                      text: "Uzbekistan",
+                    ),
+                    AreaInfoText(
+                      title: "City",
+                      text: "Tashkent",
+                    ),
+                    AreaInfoText(
+                      title: "Age",
+                      text: "24",
+                    ),
+                    Skills(),
+                    SizedBox(height: defaultPadding),
+                    Coding(),
+                    Knowledges(),
+                    Divider(),
+                    SizedBox(height: defaultPadding / 2),
+                    TextButton(
+                      onPressed: () {},
+                      child: FittedBox(
+                        child: Row(
+                          children: [
+                            Text(
+                              "DOWNLOAD CV",
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .color,
+                              ),
+                            ),
+                            SizedBox(width: defaultPadding / 2),
+                            SvgPicture.asset("assets/icons/download.svg")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: defaultPadding),
+                      color: Color(0xFF24242E),
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              _launchURL();
+
+                            },
+                            icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset("assets/icons/github.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
